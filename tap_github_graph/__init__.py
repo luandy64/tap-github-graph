58aa348bdc6e9cae02e303c2f1f252628533e9c5
Andy Lu <andy@stitchdata.com>
2019-07-14 23:08:55 +0000

    Delete unnecessary lines of code

--- tap_github_graph/__init__.py
+++ tap_github_graph/__init__.py
@@ -87,8 +87,8 @@ def format_query(stream_name, catalog_entry, cursor=None):
 
     add_cursor = ''
     if cursor:
-        # if cursor is passed in, then add_cursor gets a value that it contributes to stream_line
-        # Otherwise we interpolate in nothing
+        # if cursor is passed in, then add_cursor gets a value that it
+        # contributes to stream_line. Otherwise we interpolate in nothing
         add_cursor = 'after: "%s"' % cursor
 
     stream_line = '%s(first:1 %s)' % (stream_name, add_cursor)
@@ -97,25 +97,8 @@ def format_query(stream_name, catalog_entry, cursor=None):
 
 
 def get_all_issues(stream_name, catalog_entry, state):
-    catalog_entry = (
-        'url '
-        'labels {'
-        '  totalCount'
-        '} '
-        'repository {'
-        '  url'
-        '} '
-        'number '
-        'closedAt '
-        'title '
-        'updatedAt '
-        'authorAssociation '
-        'locked '
-        )
-
-    # TODO: Write a function to take a catalog_entry and parse it to be like this
     catalog_entry = " ".join(['url',
-                              'labels {totalCount}', # This part is the tricky bit
+                              'labels {totalCount}',
                               'repository {url}',
                               'number',
                               'closedAt',
@@ -124,284 +107,114 @@ def get_all_issues(stream_name, catalog_entry, state):
                               'authorAssociation',
                               'locked'])
 
-    query = (
-        '{' +
-        '    repository(name:"{}" owner:"{}")'.format(repo, owner) +
-        '  {' +
-        '    {}(first:1)'.format(stream_name) +
-        '    {'
-        '      pageInfo {'
-        '        hasNextPage'
-        '      }'
-        '      edges {'
-        '        cursor'
-        '        node {' +
-        catalog_entry +
-        '        }'
-        '      }'
-        '    }'
-        '  } '
-        '}')
-
-    # Best way to do this?
     query = format_query(repo, owner, stream_name, catalog_entry)
-
     response = make_graphql(query)
 
     issues_obj = response['data']['repository']['issues']
-
     has_next_page = issues_obj['pageInfo']['hasNextPage']
 
+    # Grab the object off the response. The 0 index at the end is because we
+    # only ever ask for one object at a time
+    current_object = issues_obj['edges'][0]
+
     while has_next_page:
-        #
-        # process the response
-        #
-
-        # grab the object off the response
-        # the 0 index at the end is because we only ever ask for one object at a time
-        current_object = issues_obj['edges'][0]
-
         record = current_object['node']
         pagination_cursor = current_object['cursor']
 
-        # Write record
+        # TODO: Change this to singer messages
         print(json.dumps(record))
 
-
-        # format next query
-
-        # {{ because that's how to you don't confuse python
-        add_cursor = '    issues(first:1 after: "{}") {{'.format(pagination_cursor)
-
-        query ="""
-{
-    repository(name:"tap-github" owner:"singer-io") {
-""" + add_cursor + """
-      pageInfo {
-        hasNextPage
-      }
-      edges {
-        cursor
-        node {
-          url
-          labels {
-            totalCount
-          }
-          repository {
-            url
-          }
-          number
-          closedAt
-          title
-          updatedAt
-          authorAssociation
-          locked
-        }
-      }
-    }
-  }
-}
-"""
-
         query = format_query(repo, owner, stream_name, catalog_entry, pagination_cursor)
-
-        #
-        # make the new request
-        #
         response = make_graphql(query)
 
         issues_obj = response['data']['repository']['issues']
         has_next_page = issues_obj['pageInfo']['hasNextPage']
 
-    # if we only get one object back, or we are at the end of pagination, because the object has a `hasNextPage` == false, we have to do one more processing round
-    current_object = issues_obj['edges'][0]
+    # if
+    #   1. we only get one object back
+    #   2. we are at the end of pagination, because the object has a
+    #      `hasNextPage` == false,
+    # we have to do this round of processing
     record = current_object['node']
     pagination_cursor = current_object['cursor']
 
-    # Write record
+    # TODO: Change this to singer messages
     print(json.dumps(record))
 
 
 def get_all_collaborators(stream_name, catalog_entry, state):
-
-    query = """
-{
-  repository(name: "tap-github", owner: "singer-io") {
-    collaborators(first:1) {
-      pageInfo {
-        hasNextPage
-      }
-      edges {
-        cursor
-        node {
-          login
-          id
-          url
-        }
-      }
-    }
-  }
-}
-"""
-
     catalog_entry = " ".join(['login', 'id', 'url'])
 
     query = format_query(stream_name, catalog_entry)
-
     response = make_graphql(query)
 
     collaborators_obj = response['data']['repository']['collaborators']
-
     has_next_page = collaborators_obj['pageInfo']['hasNextPage']
 
+    # Grab the object off the response. The 0 index at the end is because we
+    # only ever ask for one object at a time
+    current_object = collaborators_obj['edges'][0]
+
     while has_next_page:
-        #
-        # process the response
-        #
-
-        # grab the object off the response
-        # the 0 index at the end is because we only ever ask for one object at a time
-        current_object = collaborators_obj['edges'][0]
-
         record = current_object['node']
         pagination_cursor = current_object['cursor']
 
-        # Write record
+        # TODO: Change this to singer messages
         print(json.dumps(record))
 
-
-        # format next query
-
-        # {{ because that's how to you don't confuse python
-        add_cursor = '    collaborators(first:1 after: "{}") {{'.format(pagination_cursor)
-
-        query = """
-{
-  repository(name: "tap-github" owner: "singer-io") {
-""" + add_cursor + """
-      pageInfo {
-        hasNextPage
-      }
-      edges {
-        cursor
-        node {
-          name
-          login
-          id
-        }
-      }
-    }
-  }
-}
-"""
-
         query = format_query(stream_name, catalog_entry, pagination_cursor)
-
-        #
-        # make the new request
-        #
         response = make_graphql(query)
 
         collaborators_obj = response['data']['repository']['collaborators']
         has_next_page = collaborators_obj['pageInfo']['hasNextPage']
 
-    # if we only get one object back, or we are at the end of pagination, because the object has a `hasNextPage` == false, we have to do one more processing round
-    current_object = collaborators_obj['edges'][0]
+    # if
+    #   1. we only get one object back
+    #   2. we are at the end of pagination, because the object has a
+    #      `hasNextPage` == false,
+    # we have to do this round of processing
     record = current_object['node']
     pagination_cursor = current_object['cursor']
 
-    # Write record
+    # TODO: Change this to singer messages
     print(json.dumps(record))
 
 
-
 def get_all_assignable_users(stream_name, catalog_entry, state):
-
-    query = """
-{
-  repository(name: "tap-github" owner: "singer-io") {
-    assignableUsers(first:1) {
-      pageInfo {
-        hasNextPage
-      }
-      edges {
-        cursor
-        node {
-          name
-          login
-          id
-        }
-      }
-    }
-  }
-}
-"""
     catalog_entry = " ".join(['name', 'login', 'id'])
 
     query = format_query(stream_name, catalog_entry)
-
     response = make_graphql(query)
 
     assignable_users_obj = response['data']['repository']['assignableUsers']
-
     has_next_page = assignable_users_obj['pageInfo']['hasNextPage']
 
+    # Grab the object off the response. The 0 index at the end is because we
+    # only ever ask for one object at a time
+    current_object = assignable_users_obj['edges'][0]
+
     while has_next_page:
-        #
-        # process the response
-        #
-
-        # grab the object off the response
-        # the 0 index at the end is because we only ever ask for one object at a time
-        current_object = assignable_users_obj['edges'][0]
-
         record = current_object['node']
         pagination_cursor = current_object['cursor']
 
-        # Write record
+        # TODO: Change this to singer messages
         print(json.dumps(record))
 
-
-        # format next query
-
-        # {{ because that's how to you don't confuse python
-        add_cursor = '    assignableUsers(first:1 after: "{}") {{'.format(pagination_cursor)
-
-        query = """
-{
-  repository(name: "tap-github" owner: "singer-io") {
-""" + add_cursor + """
-      pageInfo {
-        hasNextPage
-      }
-      edges {
-        cursor
-        node {
-          name
-          login
-          id
-        }
-      }
-    }
-  }
-}
-"""
         query = format_query(stream_name, catalog_entry, pagination_cursor)
-
-        #
-        # make the new request
-        #
         response = make_graphql(query)
 
         assignable_users_obj = response['data']['repository']['assignableUsers']
         has_next_page = assignable_users_obj['pageInfo']['hasNextPage']
 
-    # if we only get one object back, or we are at the end of pagination, because the object has a `hasNextPage` == false, we have to do one more processing round
-    current_object = assignable_users_obj['edges'][0]
+    # if
+    #   1. we only get one object back
+    #   2. we are at the end of pagination, because the object has a
+    #      `hasNextPage` == false,
+    # we have to do this round of processing
     record = current_object['node']
     pagination_cursor = current_object['cursor']
 
-    # Write record
+    # TODO: Change this to singer messages
     print(json.dumps(record))
 
 
@@ -412,8 +225,8 @@ def do_discover(config):
 
 sync_stream = {
     'assignableUsers' : get_all_assignable_users,
-    'collaborators' : get_all_collaborators,
-    'issues' : get_all_issues,
+    'collaborators'   : get_all_collaborators,
+    'issues'          : get_all_issues,
 }
 
 def do_sync(catalog, state):
@@ -421,8 +234,8 @@ def do_sync(catalog, state):
 
     STREAMS = [
         'assignableUsers',
-    #    'collaborators'
-    #    'issues'
+        'collaborators',
+        'issues',
     ]
 
     for stream in STREAMS:
